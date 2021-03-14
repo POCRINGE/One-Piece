731915cec5369660a9af86c13ca4f55be488b699
aldoaprilyan3 <77969621+aldoaprilyan3@users.noreply.github.com>
2021-03-14 09:04:09 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -93,10 +93,10 @@ async def pingme(pong):
     """ For .ping command, ping the userbot from any chat.  """
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
-    await pong.edit("**𖣘**")
-    await pong.edit("**𖣘𖣘**")
-    await pong.edit("**𖣘𖣘𖣘**")
-    await pong.edit("**✦҈͜͡➳ PONG!**")
+    await pong.edit("**Pe**")
+    await pong.edit("**Sensei Mau cek**")
+    await pong.edit("**hehehe**")
+    await pong.edit("**DUARRR!!!**")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
     await pong.edit(f"**♛ ᴏɴᴇ ᴘɪᴇᴄᴇ  Ping ♛**\n"
