e01d2f19da6ea377a82afd0e3bd2ffec7837fd36
aldoaprilyan3 <77969621+aldoaprilyan3@users.noreply.github.com>
2021-03-16 20:06:49 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -120,7 +120,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "**√Repo Sensei:** [┣─[ ONE PIECE]─┫](https://github.com/aldoaprilyan3/One-Piece)\n [┣──[INSTAGRAM]──┫](instagram.com/aldoaprilyan3)"
+        " [┣─[ ONE PIECE]─┫](https://github.com/aldoaprilyan3/One-Piece)\n [┣──[INSTAGRAM]──┫](instagram.com/aldoaprilyan3)"
     )
 
 
